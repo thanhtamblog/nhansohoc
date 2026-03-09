6198aa45aba5e4e8a1e71289451e3af9724babea
thanhtamblog <mrtuan.tran79@gmail.com>
2026-03-09 17:49:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -112,3 +112,75 @@ html += `</div>`;
 document.getElementById("pitagoChart").innerHTML = html;
 }
 
+function showMissingNumbers(chart){
+
+let missing = [];
+
+for(let i=1;i<=9;i++){
+if(chart[i] == 0){
+missing.push(i);
+}
+}
+
+let text = missing.length ? missing.join(", ") : "Không có";
+
+document.getElementById("missingNumbers").innerHTML =
+"<b>Số thiếu:</b> " + text;
+
+}function showMissingNumbers(chart){
+
+let missing = [];
+
+for(let i=1;i<=9;i++){
+if(chart[i] == 0){
+missing.push(i);
+}
+}
+
+let text = missing.length ? missing.join(", ") : "Không có";
+
+document.getElementById("missingNumbers").innerHTML =
+"<b>Số thiếu:</b> " + text;
+
+}
+function detectArrows(chart){
+
+let arrows = [];
+
+if(chart[1] && chart[2] && chart[3]){
+arrows.push("Mũi tên Kế hoạch (1-2-3)");
+}
+
+if(chart[4] && chart[5] && chart[6]){
+arrows.push("Mũi tên Ý chí (4-5-6)");
+}
+
+if(chart[7] && chart[8] && chart[9]){
+arrows.push("Mũi tên Hoạt động (7-8-9)");
+}
+
+if(chart[1] && chart[4] && chart[7]){
+arrows.push("Mũi tên Thực tế (1-4-7)");
+}
+
+if(chart[2] && chart[5] && chart[8]){
+arrows.push("Mũi tên Cân bằng cảm xúc (2-5-8)");
+}
+
+if(chart[3] && chart[6] && chart[9]){
+arrows.push("Mũi tên Trí tuệ (3-6-9)");
+}
+
+document.getElementById("pitagoArrows").innerHTML =
+"<b>Mũi tên Pitago:</b><br>" + (arrows.join("<br>") || "Không có");
+
+}
+
+let chart = createPitagoChart(birth);
+
+renderPitago(chart);
+
+showMissingNumbers(chart);
+
+detectArrows(chart);
+
